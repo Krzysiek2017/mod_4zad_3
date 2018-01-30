c80de7b0d5385416dc7ca866aefac4cb910c9dcb
Krzysztof <musuashimiyamoto@interia.eu>
2018-01-30 23:28:40 +0100

    mod_4zad_3

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import style from "./TodoList.css";
+
+const TodoList = props => {
+  const lists = props.data.map(todo => (
+    <li key={todo.id}>
+      {todo.text} <button onClick={() => props.remove(todo.id)}>Done</button>
+    </li>
+  ));
+
+  return (
+    <div className={style.TodoList}>
+      <ul>{lists}</ul>
+    </div>
+  );
+};
+
+export default TodoList;
